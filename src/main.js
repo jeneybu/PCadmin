b6d917c23d7a40637240845a5e0d732a85548a44
jinli <jeneybu@163.com>
2019-09-05 16:07:23 +0800

    as

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
-import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import '@/assets/styles/reset.css'
+import '@/assets/styles/border.css'
+import Toast from '@/assets/js/toast/index'
+import { getHttp, postHttp } from './plugins/http'
+import { api } from './plugins/api'
 
-Vue.config.productionTip = false
+const VueApexCharts = require('vue-apexchart')
+Vue.component('apexchart', VueApexCharts)
+
+Vue.prototype.$get = getHttp
+Vue.prototype.$post = postHttp
+Vue.prototype.$api = api
+
+Vue.config.productionTip = false;
+Vue.use(Toast)
 
 new Vue({
-  render: h => h(App)
-}).$mount('#app')
+  router,
+  render: h => h(App),
+}).$mount('#app');
